df020bd760a368a97d424e9cee9ca889e5105978
Michiel <michiel.bellen@gmail.com>
2023-07-05 11:31:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## About The Repo
+
+This repo contains an example of minting NFT's on Cardano with PyCardano.
---
+++ main.py
@@ -0,0 +1,4 @@
+from dotenv import load_dotenv
+import mysql.connector
+from pycardano import *
+from blockfrost import BlockFrostApi, ApiError, ApiUrls,BlockFrostIPFS
